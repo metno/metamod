77eeb79859adc5c8bb468cc6a4e82c79660cd884
Heiko Klein <heikok@met.no>
2008-11-28 08:28:43 +0000

    cleanup for new version, adding licenses, removing unused/unnecessary files

--- app/damocles/stop_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-webrun_directory=[==WEBRUN_DIRECTORY==]
-continue_upload_monitor=$webrun_directory/upl/CONTINUE_UPLOAD_MONITOR
-continue_xml_import=$webrun_directory/CONTINUE_XML_IMPORT
-if [ -d $webrun_directory/upl ]; then
-   rm -f $continue_upload_monitor
-fi
-rm -f $continue_xml_import
--- app/example/stop_services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-webrun_directory=[==WEBRUN_DIRECTORY==]
-continue_upload_monitor=$webrun_directory/upl/CONTINUE_UPLOAD_MONITOR
-continue_xml_import=$webrun_directory/CONTINUE_XML_IMPORT
-rm -f $continue_upload_monitor
-rm -f $continue_xml_import
