2fc892e518f6d7a4177d04d315fd3e0bf7b23d50
Egil Stoeren <egils@met.no>
2009-06-12 12:50:43 +0000

    Added missing double apostrophe

--- search/htdocs/sch/normal.php
+++ search/htdocs/sch/normal.php
@@ -43,7 +43,7 @@
    <title><?php echo $mmConfig->getVar('SEARCH_APP_TITLE'); ?></title>
    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
    <link href="style.css" rel="stylesheet" type="text/css" />
-   <link href=<?php echo $mmConfig->getVar('LOCAL_URL'); ?>/style.css" rel="stylesheet" type="text/css" />
+   <link href="<?php echo $mmConfig->getVar('LOCAL_URL'); ?>/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <?php
