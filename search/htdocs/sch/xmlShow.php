2ea4e7ab680f7816513ff71502d719721a5b9679
Heiko Klein <heikok@met.no>
2010-02-10 14:16:26 +0000

    fetching filepath from database Dataset.DS_filePath, no longer trying to build manually

--- search/htdocs/sch/xmlShow.php
+++ search/htdocs/sch/xmlShow.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-type: text/xml');
 /*
  * Created on Jun 10, 2009
  *
@@ -36,10 +35,31 @@ header('Content-type: text/xml');
 <?php
 	require_once('../funcs/mmConfig.inc');
 	require_once ("../funcs/mmDataset.inc");
-	$filename = $mmConfig->getVar("WEBRUN_DIRECTORY").'/XML/'.$_REQUEST['dsName'];
-	list($xmdContent, $xml) = mmGetDatasetFileContent($filename);
-	if (!strlen($xml)) {
-		$xml = "<?xml version=\"1.0\" ?><error_no_file name=\"$filename\"\>";
+	
+	if (!strlen($_REQUEST['dsName'])) {
+	   echo "dsName required";
+	   exit(1);
 	}
-	echo $xml;
+		
+	// TODO: get a connection from a central function, instead of here
+	$mmDbConnection = @pg_Connect ("dbname=".$mmConfig->getVar('DATABASE_NAME')." user=".$mmConfig->getVar('PG_WEB_USER')." ".$mmConfig->getVar('PG_CONNECTSTRING_PHP'));
+   if ( !$mmDbConnection ) {
+       mmPutLog("Error. Could not connect to database: $php_errormsg");
+       $mmErrorMessage = "Error: Could not connect to database";
+	    echo $mmErrorMessage;
+		 exit(1);
+   }
+	$sqlsentence = "SELECT DS_filepath FROM DataSet WHERE DS_name = $1";
+   $result = pg_query_params($mmDbConnection, $sqlsentence, array($_REQUEST['dsName']));
+	if (!$result) {
+	   echo "Dataset ".$_REQUEST['dsName']." not found in database";
+	} else {
+		header('Content-type: text/xml');
+		list($filename) = pg_fetch_row($result);	
+		list($xmdContent, $xml) = mmGetDatasetFileContent($filename);
+		if (!strlen($xml)) {
+			$xml = "<?xml version=\"1.0\" ?><error_no_file name=\"$filename\"\>";
+		}
+		echo $xml;
+	}
 ?>
