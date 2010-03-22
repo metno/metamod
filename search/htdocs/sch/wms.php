da1c4e754b8a65f525341c2d5b17d218f1811f5d
Geir Aalberg <geira@met.no>
2010-03-22 10:43:41 +0000

    added more robust error handling on XML parsing now displays warning unless JavaScript is enabled

--- search/htdocs/sch/wms.php
+++ search/htdocs/sch/wms.php
@@ -4,6 +4,9 @@
 		<title>WMS client</title>
 		<link rel="stylesheet" href="../theme/default/style.css" type="text/css" />
 		<!--link rel="stylesheet" href="style.css" type="text/css" /-->
+		<style type="text/css">
+			#warning {font-size: x-large; padding:22 22 22 22; text-decoration: blink; color: red;}
+		</style>
 		<script src="../lib/OpenLayers.js"></script>
 
 		<script type="text/javascript">
@@ -27,6 +30,10 @@
 
 			function init(){
 				// run after document is loaded
+
+				// remove js warning
+				document.getElementById('docs').removeChild( document.getElementById('warning') );
+
 				if (wms_url != '') {
 					// do nothing if query string is empty
 					OpenLayers.loadURL(wms_url,
@@ -39,7 +46,7 @@
 					document.getElementById('shortdesc').appendChild(a);
 				}
 			}
-			
+
 			function changeLegend(event) {
 				// triggered when user changes layer
 				if (event.layer.isBaseLayer) {
@@ -48,10 +55,15 @@
 			}
 
 			function drawMap(response) {
-				
+
 				wmsParser = new OpenLayers.Format.WMSCapabilities();
-				wmsCapabilities = wmsParser.read(response.responseText);
-				
+				try {
+					// IE chokes here on bad XML, Firefox just goes on...
+					wmsCapabilities = wmsParser.read(response.responseText);
+				} catch (error) {
+					alert("Invalid Capabilities XML from mapserver:\n  " + error + "\nContent:\n  " + response.responseText);
+				}
+
 				var layersw = new OpenLayers.Control.LayerSwitcher({'autoActivate': true, 'ascending': false});
 
 				var options = {
@@ -70,9 +82,9 @@
 								new OpenLayers.Control.KeyboardDefaults()
 						],
 						numZoomLevels: 3,
-						maxResolution: 10000,
+						maxResolution: 10000
 				};
-			
+
 				// setup map and handler
 				map = new OpenLayers.Map('map', options);
 				this.map.events.register("changelayer", this, changeLegend);
@@ -88,13 +100,15 @@
 							isBaseLayer: false
 						}
 				);
-				
+
 				var layers = [];
+
 				try {
+					// This is where Firefox chokes on bad XML
 					var wms_href = wmsCapabilities.capability.request.getmap.href;
 					wmsLayers = wmsCapabilities.capability.layers;
 				} catch (error) {
-					alert("Invalid Capabilities XML from mapserver:\n" + response.responseText);
+					alert("Invalid Capabilities XML from mapserver:\n  " + error + "\nContent:\n  " + response.responseText);
 				}
 
 				// setup layers from Capabilities doc
@@ -115,15 +129,15 @@
 								legend: legend_url
 							}
 						)
-					)					
+					)
 				}
-				
+
 				layers.push(borders); // put map outlines on top
-				
+
 				map.addLayers(layers);
 				layersw.maximizeControl(true); // show layerselector maximized
 				map.zoomToExtent(map.maxExtent);
-				
+
 				// set legend URL (have to set both LAYER and LAYERS due to bug in ncWMS)
 				document.getElementById('legend').src = wmsLayers[0].styles[0].legend.href + '&LAYERS=' + wmsLayers[0].name;
 
@@ -141,7 +155,7 @@
 		<img id="legend" src="../img/blank.gif"
 				style="float: right;position:relative;top:350px;z-index:1000">
 		<div id="map" class="largemap"></div>
-		<div id="docs"></div>
+		<div id="docs"><p id="warning">Javascript must be enabled for WMS client to work!</p></div>
 		<p id="shortdesc">Data file (direct): </p>
 	</body>
 </html>
