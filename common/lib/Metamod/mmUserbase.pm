5bca2b83f233a7fc00b56fac508ad248389bc4c1
Egil Stoeren <egils@met.no>
2010-09-06 13:47:47 +0000

    Added licence

--- common/lib/Metamod/mmUserbase.pm
+++ common/lib/Metamod/mmUserbase.pm
@@ -1,10 +1,37 @@
+#----------------------------------------------------------------------------
+#  METAMOD - Web portal for metadata search and upload
+#
+#  Copyright (C) 2010 met.no
+#
+#  Contact information:
+#  Norwegian Meteorological Institute
+#  Box 43 Blindern
+#  0313 OSLO
+#  NORWAY
+#  email: Egil.Storen@met.no
+#
+#  This file is part of METAMOD
+#
+#  METAMOD is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  METAMOD is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with METAMOD; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#----------------------------------------------------------------------------
 package Metamod::mmUserbase;
 
 #
 # Class definition according to the "inside-out" method
 # described in Perl Best Practices, chapter 15.
 #
-# use Class::Std;
 use Scalar::Util;
 BEGIN { *ident = \&Scalar::Util::refaddr; }
 use DBI;
