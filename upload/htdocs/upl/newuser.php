b4823fae6e79678dfc58cb31c40c1435a245d393
Geir Aalberg <geira@met.no>
2011-01-26 13:59:56 +0000

    when sending email, read server name from config, not from url (might be a proxy server, certainly not localhost)

--- upload/htdocs/upl/newuser.php
+++ upload/htdocs/upl/newuser.php
@@ -1,32 +1,32 @@
-<?php 
-#---------------------------------------------------------------------------- 
-#  METAMOD - Web portal for metadata search and upload 
-# 
-#  Copyright (C) 2008 met.no 
-# 
-#  Contact information: 
-#  Norwegian Meteorological Institute 
-#  Box 43 Blindern 
-#  0313 OSLO 
-#  NORWAY 
-#  email: egil.storen@met.no 
-#   
-#  This file is part of METAMOD 
-# 
-#  METAMOD is free software; you can redistribute it and/or modify 
-#  it under the terms of the GNU General Public License as published by 
-#  the Free Software Foundation; either version 2 of the License, or 
-#  (at your option) any later version. 
-# 
-#  METAMOD is distributed in the hope that it will be useful, 
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of 
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-#  GNU General Public License for more details. 
-#   
-#  You should have received a copy of the GNU General Public License 
-#  along with METAMOD; if not, write to the Free Software 
-#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
-#---------------------------------------------------------------------------- 
+<?php
+#----------------------------------------------------------------------------
+#  METAMOD - Web portal for metadata search and upload
+#
+#  Copyright (C) 2008 met.no
+#
+#  Contact information:
+#  Norwegian Meteorological Institute
+#  Box 43 Blindern
+#  0313 OSLO
+#  NORWAY
+#  email: egil.storen@met.no
+#
+#  This file is part of METAMOD
+#
+#  METAMOD is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  METAMOD is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with METAMOD; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#----------------------------------------------------------------------------
 ?>
 <?php
 #
@@ -34,7 +34,7 @@
 #  users in directory $u0path. A notification E-mail is sent to the operator who
 #  has to approve the new user. Approved users have their user files moved from
 #  $u0path to $u1path. If a user file matching the user application is found in
-#  $u1path, then the user is given a new password, and the password is 
+#  $u1path, then the user is given a new password, and the password is
 #  automatically sent to the user by E-mail.
 #
    include_once("../funcs/mmConfig.inc");
@@ -71,7 +71,7 @@
          } else {
             $s1 = preg_replace(':/[^/]*/[^/]*$:','',$_SERVER["REQUEST_URI"]);
             $approvelink = 'Approve: http://' .
-                           $_SERVER["HTTP_HOST"] . $s1 . '/adm/approve.php?em=' . $normemail;
+                           $mmConfig->getVar('SERVER') . $s1 . '/adm/approve.php?em=' . $normemail;
             $matching = glob($u0path . '/' . $normemail . '.*');
             $matchingcount = count($matching);
             if ($matchingcount == 0) { // The user has not already sent a registration form
