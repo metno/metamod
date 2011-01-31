9cb21daf46a0466ff3609a916b06e4eb2e1d7c6d
Øystein Torget <oysteint@met.no>
2011-01-31 14:37:20 +0000

    Added tables that are needed for the server job queue.

--- base/userinit/createuserdb.sh
+++ base/userinit/createuserdb.sh
@@ -79,5 +79,68 @@ CREATE TABLE File (
 );
 GRANT ALL PRIVILEGES ON File TO "[==PG_WEB_USER==]";
 
+CREATE TABLE funcmap (
+        funcid SERIAL PRIMARY KEY,
+        funcname       VARCHAR(255) NOT NULL,
+        UNIQUE(funcname)
+);
+
+GRANT ALL PRIVILEGES ON funcmap TO "[==PG_WEB_USER==]";
+GRANT ALL PRIVILEGES ON funcmap_funcid_seq TO "[==PG_WEB_USER==]";
+
+CREATE TABLE job (
+        jobid           SERIAL PRIMARY KEY,
+        funcid          INT NOT NULL,
+        arg             BYTEA,
+        uniqkey         VARCHAR(255) NULL,
+        insert_time     INTEGER,
+        run_after       INTEGER NOT NULL,
+        grabbed_until   INTEGER NOT NULL,
+        priority        SMALLINT,
+        coalesce        VARCHAR(255)
+);
+
+CREATE UNIQUE INDEX job_funcid_uniqkey ON job (funcid, uniqkey);
+CREATE INDEX job_funcid_runafter ON job (funcid, run_after);
+CREATE INDEX job_funcid_coalesce ON job (funcid, coalesce);
+
+GRANT ALL PRIVILEGES ON job TO "[==PG_WEB_USER==]";
+GRANT ALL PRIVILEGES ON job_jobid_seq TO "[==PG_WEB_USER==]";
+
+CREATE TABLE note (
+        jobid           BIGINT NOT NULL,
+        notekey         VARCHAR(255),
+        PRIMARY KEY (jobid, notekey),
+        value           BYTEA
+);
+
+GRANT ALL PRIVILEGES ON note TO "[==PG_WEB_USER==]";
+
+CREATE TABLE error (
+        error_time      INTEGER NOT NULL,
+        jobid           BIGINT NOT NULL,
+        message         VARCHAR(255) NOT NULL,
+        funcid          INT NOT NULL DEFAULT 0
+);
+
+GRANT ALL PRIVILEGES ON error TO "[==PG_WEB_USER==]";
+
+CREATE INDEX error_funcid_errortime ON error (funcid, error_time);
+CREATE INDEX error_time ON error (error_time);
+CREATE INDEX error_jobid ON error (jobid);
+
+CREATE TABLE exitstatus (
+        jobid           BIGINT PRIMARY KEY NOT NULL,
+        funcid          INT NOT NULL DEFAULT 0,
+        status          SMALLINT,
+        completion_time INTEGER,
+        delete_after    INTEGER
+);
+
+CREATE INDEX exitstatus_funcid ON exitstatus (funcid);
+CREATE INDEX exitstatus_deleteafter ON exitstatus (delete_after);
+
+GRANT ALL PRIVILEGES ON exitstatus TO "[==PG_WEB_USER==]";
+
 \q
 EOF
