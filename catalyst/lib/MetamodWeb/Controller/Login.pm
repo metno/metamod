04a93cfd4f46a2633669501b735491ef2cc1ace4
Øystein Torget <oysteint@met.no>
2011-02-24 12:32:54 +0000

    Added controller actions for requesting the registration of a new user.

--- catalyst/lib/MetamodWeb/Controller/Login.pm
+++ catalyst/lib/MetamodWeb/Controller/Login.pm
@@ -23,9 +23,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 use Moose;
 use namespace::autoclean;
 
-use MetamodWeb::Utils::UI::Login;
+use Data::FormValidator::Constraints qw(:closures);
 
-BEGIN {extends 'Catalyst::Controller'; }
+use Metamod::Email;
+use MetamodWeb::Utils::UI::Login;
+use MetamodWeb::Utils::FormValidator;
+
+BEGIN {extends 'MetamodWeb::BaseController::Base'; }
 
 =head1 NAME
 
@@ -47,9 +51,19 @@ Controller specific initialisation.
 sub auto :Private {
     my ( $self, $c ) = @_;
 
-    my $mm_config = $c->stash->{ mm_config }; my $ui_utils =
-    MetamodWeb::Utils::UI::Login->new( { config => $mm_config, c => $c } );
+    my $mm_config = $c->stash->{ mm_config };
+    my $ui_utils = MetamodWeb::Utils::UI::Login->new( { config => $mm_config, c => $c } );
     $c->stash( login_ui_utils => $ui_utils, );
+
+    # If the valiation failed on the previous page we store that in the flash.
+    # We cannot save the validator in the flash since it can contain code references
+    # that are not handled by the Perl serialiser.
+    my $validation_failure = $c->flash->{validation_failure};
+    if( $validation_failure ){
+        $self->validate_new_user($c);
+    }
+
+    return 1;
 
 }
 
@@ -59,7 +73,7 @@ Action for display the login form.
 
 =cut
 
-sub index :Path :Args(0) {
+sub index :Path('/login') :Args(0) {
     my ($self, $c) = @_;
 
     $c->stash( template => 'login.tt' );
@@ -98,10 +112,10 @@ sub authenticate :Path('authenticate') :Args(0) {
             $c->response->redirect($c->uri_for($return_path) . "?$return_params" );
             return;
         } else {
-            $c->stash(error_msg => "Invalid username or password.");
+            $self->add_error_msgs($c, 'Invalid username or password.' );
         }
     } else {
-        $c->stash(error_msg => "Empty username or password.");
+        $self->add_error_msgs($c, 'Empty username or password.' );
     }
 
     $c->forward('index');
@@ -116,8 +130,152 @@ Action for registering a new user in the system.
 sub register : Path('register') :Args(0) {
     my ($self, $c) = @_;
 
-    # not yet implemented to just send back to login
-    $c->forward('index');
+    my $result = $self->validate_new_user($c);
+    if( !$result->success() ){
+        $c->flash( validation_failure => 1 );
+        return $c->res->redirect($c->uri_for('/login', $c->req->params ) );
+    }
+
+    my $mm_config = $c->stash->{mm_config};
+    my $valid_fields = $result->valid();
+
+    my $institution = '';
+    if( exists $valid_fields->{institution_name} && $valid_fields->{institution_name} eq 'other' ){
+        $institution = $valid_fields->{institution_other};
+    } else {
+        $institution = $valid_fields->{institution_name};
+    }
+
+    my $users = $c->model('Userbase::Usertable')->search( { u_loginname => $valid_fields->{username} } );
+    if( $users->count() != 0 ){
+        $self->add_error_msgs($c, "The username has already been taken. Please choose another" );
+        return $c->res->redirect($c->uri_for('/login', $c->req->params ));
+    }
+
+    my $user_values = {
+        a_id => $mm_config->get('APPLICATION_ID'),
+        u_name => $valid_fields->{realname},
+        u_email => $valid_fields->{email},
+        u_loginname => $valid_fields->{username},
+        u_institution => $institution,
+        u_telephone => $valid_fields->{telephone},
+    };
+
+    my @roles = ();
+    if( $valid_fields->{access_rights } eq 'subscription' ){
+        @roles = qw(subscription);
+    } elsif( $valid_fields->{access_rights } eq 'upload' ){
+        @roles = qw(subscription upload);
+    }
+
+    my $new_user = $c->model('Userbase::Usertable')->new_user($user_values, \@roles);
+
+    $self->send_user_receipt($c, $user_values);
+
+    $self->send_operator_email($c, $new_user);
+
+    $self->add_info_msgs($c, 'New user has been request. A receipt has been sent to your email');
+    $c->res->redirect($c->uri_for('/login', $c->req->params ));
+
+}
+
+sub validate_new_user : Private {
+    my ($self, $c) = @_;
+
+    my %form_profile = (
+        required => [qw( username email access_rights realname )],
+        optional => [qw( institution_name institution_other telephone ) ],
+        constraint_methods => {
+            email => email(),
+            username => sub { # use a sub so we can set the constraint name and a message
+                my ($dfv, $val) = @_;
+                $dfv->set_current_constraint_name('username_constraint');
+                return $val =~ /^\w{3,20}$/;
+            },
+        },
+        labels => {
+            username => 'Username',
+            email => 'Email',
+            access_right => 'Access rights',
+            institution => 'Institution name',
+            institution_other => 'Institution name (other)',
+            telephone => 'Telephone',
+            realname => 'Name',
+        },
+        msgs => sub {
+            email => 'Invalid email address',
+            username_constraint => "Invalid username. Only user letters, '_' and numbers",
+        }
+    );
+    my $validator = MetamodWeb::Utils::FormValidator->new( validation_profile => \%form_profile );
+    my $result = $validator->validate($c->req->params);
+    $c->stash( validator => $validator );
+
+    return $result;
+
+}
+
+sub send_user_receipt {
+    my ($self, $c, $user_info) = @_;
+
+    my $mm_config = $c->stash->{mm_config};
+    my $operator_email = $mm_config->get('OPERATOR_EMAIL');
+    my $application_name = $mm_config->get('APPLICATION_NAME');
+    my $signature = $mm_config->get('EMAIL_SIGNATURE') || '';
+
+    my $email_body = <<"END_BODY";
+Dear $user_info->{u_name},
+
+we have received your request for a new user with the following information.
+
+Name: $user_info->{u_name}
+Email: $user_info->{u_email}
+Username: $user_info->{u_loginname}
+Institution: $user_info->{u_institution}
+Telephone: $user_info->{u_telephone}
+
+You will receive an email with your password as soon as your request has been manually reviewed.
+
+$signature
+END_BODY
+
+    Metamod::Email::send_simple_email(
+        to => [ $user_info->{u_email} ],
+        from => $operator_email,
+        subject => "$application_name new user request",
+        body => $email_body,
+    );
+
+    return;
+
+}
+
+sub send_operator_email {
+    my ($self, $c, $new_user) = @_;
+
+    my $mm_config = $c->stash->{mm_config};
+    my $base_url = $mm_config->get('BASE_PART_OF_EXTERNAL_URL');
+    my $local_url = $mm_config->get('LOCAL_URL');
+    my $approve_url = "${base_url}${local_url}admin/confirm_user/" . $new_user->u_id();
+
+    my $email_body = <<"END_BODY";
+A new user has been registred. Please check the information
+and approve the user if the information is ok.
+
+$approve_url
+END_BODY
+
+    my $operator_email = $mm_config->get('OPERATOR_EMAIL');
+    my $application_name = $mm_config->get('APPLICATION_NAME');
+
+    Metamod::Email::send_simple_email(
+        to => [ $operator_email ],
+        from => 'dummy@example.com',
+        subject => "$application_name new user registred",
+        body => $email_body,
+    );
+
+    return;
 
 }
 
