e6e2d7e802c1d17833495364c1c6f21481059b6b
Øystein Torget <oysteint@met.no>
2011-03-21 11:49:43 +0000

    Added support for determinig the data_file_location when EXTERNAL_REPOSITORY is true.

--- base/scripts/write_data_file_location.pl
+++ base/scripts/write_data_file_location.pl
@@ -6,6 +6,7 @@ use warnings;
 use FindBin;
 
 use lib "$FindBin::Bin/../../common/lib";
+use lib "$FindBin::Bin/../../catalyst/lib";
 
 use Cwd qw(abs_path);
 use File::Find;
@@ -14,6 +15,7 @@ use Log::Log4perl qw(:easy);
 use Pod::Usage;
 
 use Metamod::Dataset;
+use MetamodWeb::Schema::Userbase;
 
 =head1 NAME
 
@@ -44,6 +46,16 @@ if( @ARGV != 1 ){
 my $dirname = shift @ARGV;
 my $config = Metamod::Config->new();
 
+
+my $userbase_model;
+my $external_repository = ($config->get('EXTERNAL_REPOSITORY') eq 'true') ? 1 : 0;
+if( $external_repository ){
+    my $userbase_dsn = $config->getDSN_Userbase();
+
+    $userbase_model = MetamodWeb::Schema::Userbase->connect($userbase_dsn, 'admin');
+    $userbase_model->storage()->ensure_connected();
+
+}
 
 # We check if we can reach the data store. If that is not possible it is not
 # possible to check that the parsed path is available and we don't want to
@@ -81,7 +93,43 @@ foreach my $file (sort @metadata_files){
         next;
     }
 
-    my $datafile_path = "${opendap_directory}/$location";
+    my $datafile_path;
+    if( $external_repository ){
+
+        my $external_location = get_external_location($dataset, $userbase_model);
+
+        next if !defined $external_location;
+
+        # location should match part of the external location. We want the part
+        # of location that comes after the match with external location.
+        my @location_dirs = File::Spec->splitdir($location);
+        my @external_dirs = File::Spec->splitdir($external_location);
+
+        # find the first matching directory. We match agains the last dir in the external path
+        my $location_index = 0;
+        foreach my $dir (@location_dirs){
+
+            $location_index++;
+
+            last if $dir eq $external_dirs[-1];
+
+        }
+
+        my @location_rest;
+
+        # no common directories. Just append to the end
+        if( $location_index == scalar @location_dirs ){
+            @location_rest = @location_dirs;
+        } else {
+           @location_rest = @location_dirs[$location_index .. $#location_dirs];
+        }
+
+        $datafile_path = File::Spec->catfile( $external_location, @location_rest );
+
+    } else {
+        $datafile_path = "${opendap_directory}/$location";
+    }
+
     if( ! -e $datafile_path ){
         $logger->error("$datafile_path does not exist");
         next;
@@ -124,5 +172,40 @@ sub dataref_to_location {
         $logger->error( "Failed to parse: $dataref" );
         return;
     }
+
+}
+
+sub get_external_location {
+    my ($dataset, $model) = @_;
+
+    my %info = $dataset->getInfo();
+    my $name = $info{name};
+
+    my $app_id;
+    my $ds_name;
+
+    # Only level 1 datasets are found in the data userbase so we drop the last
+    # part of the dataset name as it is not relevant for the userbase query.
+    if( $name =~ /^(.+?)\/(.+?)\/.+$/ ){
+        $app_id = $1;
+        $ds_name = $2;
+    } else {
+        $logger->error("Failed to parse the dataset name: $name");
+        return;
+    }
+
+    my $userbase_ds = $model->resultset('Dataset')->search({ a_id => $app_id, ds_name => $ds_name } )->first();
+    if( !defined $userbase_ds ){
+        $logger->warn("Could not find dataset in the userbase: a_id : '$app_id', ds_name : '$ds_name'" );
+        return;
+    }
+
+    my $location = $userbase_ds->infods()->search({i_type => 'LOCATION' } )->first();
+    if( !defined $location ){
+        $logger->warn("Could not find location for dataset: a_id : '$app_id', ds_name : '$ds_name'");
+        return;
+    }
+
+    return $location->i_content();
 
 }
