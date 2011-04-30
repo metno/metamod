82f4fa2547d5c71753e8f17a71571087289625b9
Heiko Klein <heikok@met.no>
2011-04-30 13:21:59 +0000

    change docs for master_config.txt installing correct rc2.d scripts

--- common/install_jobs.sh
+++ common/install_jobs.sh
@@ -8,7 +8,7 @@ ordie () {
     if [ $? != 0 ]
     then
         echo "Please remove target file(s) before trying automatic install:"
-        echo "sudo rm /etc/default/$CATALYST_APP /etc/init.d/$CATALYST_APP /etc/init.d/metamodServices-[==APPLICATION_ID==] /etc/rc2.d/$CATALYST_APP /etc/apache2/conf.d/[==APPLICATION_ID==]"
+        echo "sudo rm /etc/default/$CATALYST_APP /etc/init.d/$CATALYST_APP /etc/init.d/metamodServices-[==APPLICATION_ID==] /etc/rc2.d/S92$CATALYST_APP /etc/apache2/conf.d/[==APPLICATION_ID==]"
         exit 1
     fi
 }
@@ -26,7 +26,7 @@ fi
 sudo ln -s [==TARGET_DIRECTORY==]/etc/default/catalyst-myapp /etc/default/$CATALYST_APP; ordie
 sudo ln -s [==TARGET_DIRECTORY==]/etc/init.d/metamod-catalyst /etc/init.d/$CATALYST_APP; ordie
 # start Catalyst at boot
-sudo ln -s /etc/init.d/$CATALYST_APP /etc/rc2.d/$CATALYST_APP; ordie
+sudo ln -s /etc/init.d/$CATALYST_APP /etc/rc2.d/S92$CATALYST_APP; ordie
 
 
 # install metamodInit.sh job [code copied from Egil]
@@ -35,10 +35,10 @@ if [ $APPLICATION_USER ]; then
 	cat > /tmp/metamodServices-[==APPLICATION_ID==] <<- EOT
 		#! /bin/sh
 		su -c "[==TARGET_DIRECTORY==]/metamodInit.sh $*" -s /bin/sh $APPLICATION_USER
-		# not sure if this will work since you need the password for APPLICATION_USER to su (rewrite to sudo?)
 	EOT
 	# make sure the tabs above are not replaced with spaces (or the script will break)
 	sudo mv /tmp/metamodServices-[==APPLICATION_ID==] /etc/init.d/; ordie
+    sudo ln -s /etc/init.d/metamodServices-[==APPLICATION_ID==] /etc/rc2.d/S99metamodServices-[==APPLICATION_ID==]; ordie
 fi
 
 # install config link
