49a0f624cb2b43afd9b55dd6e6d1c61a1a5c45c5
Geir Aalberg <geira@met.no>
2011-05-26 11:30:53 +0000

    no longer installing links to master_config in /etc since obsoleted

--- common/install_jobs.sh
+++ common/install_jobs.sh
@@ -41,6 +41,6 @@ if [ $APPLICATION_USER ]; then
     sudo ln -s /etc/init.d/metamodServices-[==APPLICATION_ID==] /etc/rc2.d/S99metamodServices-[==APPLICATION_ID==]; ordie
 fi
 
-# install config link
-sudo mkdir -p /etc/metamod-[==MAJORVERSION==]
-sudo ln -s [==TARGET_DIRECTORY==]/master_config.txt  /etc/metamod-[==MAJORVERSION==]/[==APPLICATION_ID==].cfg; ordie
+# install config link (no longer in use?)
+#sudo mkdir -p /etc/metamod-[==MAJORVERSION==]
+#sudo ln -s [==TARGET_DIRECTORY==]/master_config.txt  /etc/metamod-[==MAJORVERSION==]/[==APPLICATION_ID==].cfg; ordie
