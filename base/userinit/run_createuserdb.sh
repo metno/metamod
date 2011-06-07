b4f85ce420f8ac3c15392394193891685a938e08
Øystein Torget <oysteint@met.no>
2011-06-07 08:45:23 +0000

    Changed user database initialisation to use dynamic configuration instead of update_target process.

--- base/userinit/run_createuserdb.sh
+++ base/userinit/run_createuserdb.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Re-initialize the User database. NOTE: All existing data will be lost!
 #
+
+if [ $# != 1 ]
+then
+    echo "You must supply the master config file as a parameter"
+    exit 1
+fi
+
+if [ ! -r $1 ]
+then
+    echo "Cannot read the file "$1
+    exit 1
+fi
+
 exec >run_createuserdb.out 2>&1
 echo "------------ Reinitialize the user database:"
-. ./createuserdb.sh
+. ./createuserdb.sh $1
