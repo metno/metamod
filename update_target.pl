0cb1f1580e5070d3783c7f3f34e0982a0810c683
Øystein Torget <oysteint@met.no>
2011-08-18 07:59:31 +0000

    Removed the use of LoggerConfigParser. Logger configuration should now be written directly as Log4Perl configuration.

--- update_target.pl
+++ update_target.pl
@@ -43,7 +43,6 @@ use FindBin qw($Bin);
 use Cwd;
 
 use lib "$Bin/common/lib";
-use Metamod::LoggerConfigParser;
 
 #
 #  Check command line arguments (FIXME: rewrite using Getopt::Long)
@@ -318,10 +317,6 @@ foreach my $filelistpath (@flistpathes) {
    close (FILES);
 }
 
-
-my $logger_config = File::Spec->catfile( $targetdir, 'logger_config.ini' );
-my $lcp = Metamod::LoggerConfigParser->new( { verbose => 1 } );
-$lcp->create_and_write_configs($configfile,$logger_config);
 
 if ($missing_variables > 0) {
    print "NOTE: All [==...==] constructs found that were not defined in the configuration file\n" .
