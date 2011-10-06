369e1543c90d3b235776cba1a156627bc9bcff91
Øystein Torget <oysteint@met.no>
2011-10-06 08:53:37 +0000

    Removed dependency on METAMOD_UPLOADDIRECTORY for creating upl/ and XML/ directory.

--- common/prepare_runtime_env.sh
+++ common/prepare_runtime_env.sh
@@ -24,34 +24,35 @@ mkdir -p $WEBRUN_DIRECTORY
 #
 mkdir -p $WEBRUN_DIRECTORY/download
 
-if [ "$METAMODUPLOAD_DIRECTORY" != "" ]; then
-   mkdir -p $WEBRUN_DIRECTORY/upl
-   mkdir -p $WEBRUN_DIRECTORY/upl/problemfiles
-   mkdir -p $WEBRUN_DIRECTORY/upl/uerr
-   mkdir -p $WEBRUN_DIRECTORY/upl/ftaf
-   mkdir -p $WEBRUN_DIRECTORY/upl/etaf
-   if [ ! -f $WEBRUN_DIRECTORY/ftp_events ]; then
-      cat >$WEBRUN_DIRECTORY/ftp_events <<EOF
+
+mkdir -p $WEBRUN_DIRECTORY/upl
+mkdir -p $WEBRUN_DIRECTORY/upl/problemfiles
+mkdir -p $WEBRUN_DIRECTORY/upl/uerr
+mkdir -p $WEBRUN_DIRECTORY/upl/ftaf
+mkdir -p $WEBRUN_DIRECTORY/upl/etaf
+if [ ! -f $WEBRUN_DIRECTORY/ftp_events ]; then
+    cat >$WEBRUN_DIRECTORY/ftp_events <<EOF
 $FTP_EVENTS_INITIAL_CONTENT
 EOF
-   fi
+fi
+
 #
 #  Initialise XML directory:
 #
-   mkdir -p $WEBRUN_DIRECTORY/XML/$APPLICATION_ID
-   mkdir -p $WEBRUN_DIRECTORY/XML/history
+mkdir -p $WEBRUN_DIRECTORY/XML/$APPLICATION_ID
+mkdir -p $WEBRUN_DIRECTORY/XML/history
+
 #
 #  Initialize upload and OPeNDAP directories:
 #
-   if [ '$UPLOAD_DIRECTORY' != '' ]; then mkdir -p $UPLOAD_DIRECTORY; fi
-   if [ '$UPLOAD_FTP_DIRECTORY' != '' ]; then mkdir -p $UPLOAD_FTP_DIRECTORY; fi
-   if [ '$OPENDAP_DIRECTORY' != '' ]; then
-      mkdir -p $OPENDAP_DIRECTORY
-      if [ -w $OPENDAP_DIRECTORY -a ! -f $OPENDAP_DIRECTORY/.htaccess ]; then
-         cat >$OPENDAP_DIRECTORY/.htaccess <<EOF
+if [ '$UPLOAD_DIRECTORY' != '' ]; then mkdir -p $UPLOAD_DIRECTORY; fi
+if [ '$UPLOAD_FTP_DIRECTORY' != '' ]; then mkdir -p $UPLOAD_FTP_DIRECTORY; fi
+if [ '$OPENDAP_DIRECTORY' != '' ]; then
+    mkdir -p $OPENDAP_DIRECTORY
+    if [ -w $OPENDAP_DIRECTORY -a ! -f $OPENDAP_DIRECTORY/.htaccess ]; then
+        cat >$OPENDAP_DIRECTORY/.htaccess <<EOF
 Order Deny,Allow
 Deny from all
 EOF
-      fi
-   fi
+    fi
 fi
