cc67c8795f27d64f6ba5d268b9b429ff57a3e5af
Geir Aalberg <geira@met.no>
2014-05-23 09:00:31 +0000

    now runs from outside of dir, takes multiple file parameters, more docs

--- upload/scripts/add_file_to_queue.pl
+++ upload/scripts/add_file_to_queue.pl
@@ -39,24 +39,32 @@ use strict;
 use warnings;
 use File::Spec;
 use Getopt::Long;
+use Pod::Usage;
 
-# small routine to get lib-directories relative to the installed file
-sub getTargetDir {
-    my ($finalDir) = @_;
-    my ( $vol, $dir, $file ) = File::Spec->splitpath(__FILE__);
-    $dir = $dir ? File::Spec->catdir( $dir, ".." ) : File::Spec->updir();
-    $dir = File::Spec->catdir( $dir, $finalDir );
-    return File::Spec->catpath( $vol, $dir, "" );
-}
+use FindBin;
+use lib ("$FindBin::Bin/../../common/lib", '.');
 
-use lib ( getTargetDir('lib'), getTargetDir('scripts'), '../../common/lib' );
 use TheSchwartz;
 use Metamod::Queue;
 use Metamod::Config;
 
+=head1 NAME
+
+B<add_file_to_queue.pl> - enter job for upload_monitor to process given file
+
+=head1 DESCRIPTION
+
+Used for re-processing uploaded NetCDF files (or tarballs of such), or for testing
+
+=head1 USAGE
+
+  add_file_to_queue.pl [ --config <dir> ] <ncfile> ...
+
+=cut
+
 # Parse cmd line params
 my $config_file_or_dir;
-GetOptions ('config=s' => \$config_file_or_dir) or usage();
+GetOptions ('config=s' => \$config_file_or_dir) or pod2usage(1);
 
 if(!Metamod::Config->config_found($config_file_or_dir)){
     print STDERR "Could not find the configuration on the commandline or the in the environment\n";
@@ -66,20 +74,12 @@ my $mm_config = Metamod::Config->new($config_file_or_dir);
 
 my $queue = Metamod::Queue->new( 'mm_config' => $mm_config);
 
-if (scalar @ARGV != 1) {
-   die "\nUsage:\n\n     $0 abs_path_to_ncfile\n\n";
-}
-my $abs_path_to_ncfile = $ARGV[0];
-my $success = $queue->insert_job(
-    job_type => 'Metamod::Queue::Worker::Upload',
-    job_parameters => { file => "$abs_path_to_ncfile", type => 'INDEX' }, # TAF, FTP
-);
+pod2usage(1) unless @ARGV;
 
-if( ! $success ){
-    die "Could not add $abs_path_to_ncfile to processing queue!\n";
-}
-
-sub usage {
-    print "usage: $0 [ --config <configpath> ] [ --pid <pidfile> ] [ --log <logfile> ] \n";
-    exit 1;
+foreach my $ncfile (@ARGV) {
+    $queue->insert_job(
+        job_type => 'Metamod::Queue::Worker::Upload',
+        job_parameters => { file => "$ncfile", type => 'INDEX' }, # what about TAF, FTP?
+    ) && print STDERR "File $ncfile added to processing queue.\n"
+    or die "Could not add $ncfile to processing queue!\n";
 }
