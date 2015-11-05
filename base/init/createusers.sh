40859211112d71b44573cfdada09749a0858de1d
Geir Aalberg <geira@met.no>
2015-11-05 17:51:58 +0000

    proj4 has changed packet name in trusty trying to get createusers.sh to run on remote db host

--- base/init/createusers.sh
+++ base/init/createusers.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------------------------------
+#
+# BEFORE RUNNING THIS SCRIPT, MAKE SURE YOU HAVE THE NECESSARY PERMISSIONS TO MANAGE THE DATABASE!
+#
+# If running on localhost, you must edit pg_hba.conf as described in installation docs.
+#
+# TODO: Implement .pgpass file to store passwords - http://www.postgresql.org/docs/9.3/static/libpq-pgpass.html
+#
+# ------------------------------------------------------------------------------------------------
+
 SCRIPT_PATH="`dirname \"$0\"`"
 CONFIG=$1
 # config must be set in $METAMOD_MASTER_CONFIG envvar if not given as command line param
@@ -24,14 +34,14 @@ check "PG_WEB_USER"
 # must not die on fail as happens when user already exists
 
 echo "Creating admin user $PG_ADMIN_USER"
-createuser -e --username=postgres --adduser --createrole --createdb --superuser $PG_ADMIN_USER
+createuser -e ${PG_HOST:+"--host"} $PG_HOST ${PG_PORT:+"--host"} $PG_PORT --username=postgres --adduser --createrole --createdb --superuser $PG_ADMIN_USER
 #ordie "Couldn't create user $PG_ADMIN_USER"
 
 # also find a way to set passwd
 # postgres=# alter user admin with password 'admin';
 
 echo "Creating web user $PG_WEB_USER"
-createuser -e --username=postgres --no-adduser --no-createrole --no-createdb $PG_WEB_USER
+createuser -e ${PG_HOST:+"--host"} $PG_HOST ${PG_PORT:+"--host"} $PG_PORT --username=postgres --no-adduser --no-createrole --no-createdb $PG_WEB_USER
 #ordie "Couldn't create user $PG_WEB_USER"
 
 # we don't care if createuser failed
